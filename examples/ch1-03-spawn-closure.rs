aa71fd9fbfb7d273faf732987d50a672f608363b
Mara Bos <m-ou.se@m-ou.se>
2023-01-26 14:30:29 +0100

    Fix typo in example.

--- examples/ch1-03-spawn-closure.rs
+++ examples/ch1-03-spawn-closure.rs
@@ -4,7 +4,7 @@ fn main() {
     let numbers = vec![1, 2, 3];
 
     thread::spawn(move || {
-        for n in numbers {
+        for n in &numbers {
             println!("{n}");
         }
     }).join().unwrap();
